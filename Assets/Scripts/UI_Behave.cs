db4f0b9d37b2ac10a5a59a369844b6394c9cc44a
Sakari-Ojamaa <sakkoja@gmail.com>
2021-06-27 20:21:38 +0300

    Added UI for answering the games questions
    Added UI for true/false as well as multiple choice answers.
    Made UI elements appear and disappear as required.

--- Assets/Scripts/UI_Behave.cs
+++ Assets/Scripts/UI_Behave.cs
@@ -15,13 +15,19 @@ public class UI_Behave : MonoBehaviour
     public Dropdown Category, Difficulty;
     public InputField QuestionCountField;
 
-    
+    public Button True, False, Submit, Answer1, Answer2, Answer3;
+    public InputField Question, Answer;
+
+    public Text GM, CAT, Count, Diff, Star, Que, Ans;
+    //public List<Text> TextList = new List<Text>();
+
     public string TokenDebug;
     public List<category> catDebug;
     private List<string> cats;
     //private WWWForm ParamForm = new WWWForm();
     TokenClass myToken = new TokenClass();
     Categories AllCategories = new Categories();
+    questionData questionsDataList = new questionData();
     WWWForm ParamForm;
 
     private int diffNumber = 0;
@@ -29,6 +35,7 @@ public class UI_Behave : MonoBehaviour
     private int roundCount = 10;
     private int categoryPH = 0;
     private string typePH = "multiple";
+    private bool typeSwitch = true;
 
     //https://opentdb.com/api.php?amount=7&category=9&difficulty=medium&type=multiple
     private void Awake()
@@ -54,13 +61,33 @@ public class UI_Behave : MonoBehaviour
         //{ setCategoryForm(Category.itemText.text); });
         Category.onValueChanged.AddListener(delegate { setCategoryForm(Category.value); }); //Listener will pass index selected item
         Difficulty.onValueChanged.AddListener(delegate { setDifficultyForm(Difficulty.value); });
+
+        StartButton.gameObject.SetActive(false);
+        GameModeButton1.gameObject.SetActive(false);
+        GameModeButton2.gameObject.SetActive(false);
+
+        Category.gameObject.SetActive(false);
+        Difficulty.gameObject.SetActive(false);
+        QuestionCountField.gameObject.SetActive(false);
+
+        SetButtons(false); 
+
+        GM.enabled = false;
+        CAT.enabled = false;
+        Count.enabled = false;
+        Diff.enabled = false;
+        Star.enabled = false;
+        Que.enabled = false;
+        Ans.enabled = false;
     }
 
     void NewGame()
     {
         Debug.Log("New Game!");
         StartCoroutine(GetToken(tokenStorage)); //Coroutine for IEnumerator
-        
+        GM.enabled = true;
+        GameModeButton1.gameObject.SetActive(true);
+        GameModeButton2.gameObject.SetActive(true);
     }
     public void tokenStorage(string fromweb)
     {
@@ -92,6 +119,17 @@ public class UI_Behave : MonoBehaviour
     {
         if (i == 0) typePH = "multiple";//ParamForm.AddField("type", "multiple");
         else typePH = "boolean"; //ParamForm.AddField("type", "bool");
+
+        CAT.enabled = true;
+        Count.enabled = true;
+        Diff.enabled = true;
+
+        Category.gameObject.SetActive(true);
+        Difficulty.gameObject.SetActive(true);
+        QuestionCountField.gameObject.SetActive(true);
+
+        Star.enabled = true;
+        StartButton.gameObject.SetActive(true);
     }
     void setCount(int i)
     {
@@ -167,10 +205,53 @@ public class UI_Behave : MonoBehaviour
         ParamForm.AddField("type", typePH);
 
         StartCoroutine(SendParam(StartReturn));
+        SetButtons(true, typeSwitch);
     }
     void StartReturn(string fromweb)
     {
+        questionData data = new questionData();
+        data = JsonUtility.FromJson<questionData>(fromweb);
+        questionsDataList = data;
         Debug.Log(fromweb);
+    }
+    void SetButtons(bool to)
+    {
+        Que.enabled = to;
+        Ans.enabled = to;
+
+        Question.gameObject.SetActive(to);
+
+        Answer.gameObject.SetActive(to);
+        Submit.gameObject.SetActive(to);
+
+        True.gameObject.SetActive(to);
+        False.gameObject.SetActive(to);
+        
+        Answer1.gameObject.SetActive(to);
+        Answer2.gameObject.SetActive(to);
+        Answer3.gameObject.SetActive(to);
+
+    }
+    void SetButtons(bool to, bool GM)
+    {
+        Que.enabled = to;
+        Ans.enabled = to;
+
+        Question.gameObject.SetActive(to);
+
+        if (GM)
+        {
+            //Answer.gameObject.SetActive(to);
+            //Submit.gameObject.SetActive(to);
+            Answer1.gameObject.SetActive(GM);
+            Answer2.gameObject.SetActive(GM);
+            Answer3.gameObject.SetActive(GM);
+        }
+        else
+        {
+            True.gameObject.SetActive(to);
+            False.gameObject.SetActive(to);
+        }
     }
 
     IEnumerator GetToken(System.Action<string> gibtoken) //With callback
@@ -200,12 +281,11 @@ public class UI_Behave : MonoBehaviour
 
     IEnumerator SendParam(System.Action<string> gibAny) //WIP
     {
-        //https://opentdb.com/api.php?amount=7&category=9&difficulty=medium&type=multiple
-        //https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=boolean
         string sendToken = TokenDebug;
        
         string uriA = "https://opentdb.com/api.php?";
 
+        //Scuffed uri generation, just patching strings together.
         string uriB = uriA + "&" +"amount="+ roundCount;
         if (categoryPH != 0)
         {
@@ -216,11 +296,7 @@ public class UI_Behave : MonoBehaviour
             uriB = uriB + "&" + "difficulty=" + diffText;
         }        
         uriB = uriB + "&" + "type=" + typePH + "&" + "token=" + TokenDebug;
-        Debug.Log(uriB);
-        yield return "";
-        //uriB = uriA + "&" + roundCount + "&" + categoryPH + "&" + diffText;
-
-        
+        Debug.Log(uriB);        
         using (UnityWebRequest www = UnityWebRequest.Get(uriB)) //Let Unity handle formatting for passing parameters of game
         {
             yield return www.SendWebRequest();
@@ -236,8 +312,8 @@ public class UI_Behave : MonoBehaviour
                     break;
                 case UnityWebRequest.Result.Success:
 
-                    Debug.Log("Form upload complete!");
-
+                    Debug.Log("Upload complete!");
+                    /*
                     Debug.Log("Text lenth:"+www.downloadHandler.text.Length);
                     Debug.Log("Data:" + www.downloadHandler.data);
                     Debug.Log("Text:" + www.downloadHandler.text.ToString());
@@ -245,70 +321,14 @@ public class UI_Behave : MonoBehaviour
                     Debug.Log("Result:" + www.result);
                     Debug.Log("URI:" + www.uri);
                     Debug.Log("URL:" + www.url);
-
+                    */
                     string returnString = www.downloadHandler.text;
-                    Debug.Log(":\nReceived: " + returnString);
+                    //Debug.Log(":\nReceived: " + returnString);
                     gibAny(returnString);
-
                     break;
-            }
-            
-        /*
-        using (UnityWebRequest www = UnityWebRequest.Post("https://opentdb.com/api.php?", ParamForm)) //Let Unity handle formatting for passing parameters of game
-        {
-            yield return www.SendWebRequest();
-
-            Debug.Log(ParamForm);
-            Debug.Log(ParamForm.data);
-            Debug.Log(ParamForm.headers);
-            switch (www.result)
-            {
-                case UnityWebRequest.Result.ConnectionError:
-                case UnityWebRequest.Result.DataProcessingError:
-                    Debug.LogError(": Error: " + www.error);
-                    break;
-                case UnityWebRequest.Result.ProtocolError:
-                    Debug.LogError(": HTTP Error: " + www.error);
-                    break;
-                case UnityWebRequest.Result.Success:
-
-                    Debug.Log("Form upload complete!");
-
-                    Debug.Log(www.downloadHandler.text.Length);
-                    Debug.Log(www.downloadHandler.data);
-                    Debug.Log(www.downloadHandler.text.ToString());
-                    Debug.Log(www);
-                    Debug.Log(www.result);
-                    Debug.Log(www.uri);
-                    Debug.Log(www.url);
-
-                    string returnString = www.downloadHandler.text;
-                    Debug.Log(":\nReceived: " + returnString);
-                    gibAny(returnString);
-
-                    break;
-            }
-            */
-
-        /*
-        if (www.result != UnityWebRequest.Result.Success)
-        {
-            Debug.Log(www.error);
-        }
-        else
-        {
-            Debug.Log("Form upload complete!");
-            Debug.Log(www.downloadHandler.text);
-            Debug.Log(www);
-            Debug.Log(www.result);
-            Debug.Log(www.uri);
-            Debug.Log(www.url);
-            gibAny(www.downloadHandler.text);
-        }
-        */
+            }           
 
         }
-
 
     }
 
